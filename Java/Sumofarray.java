837d51521010ed2736f2f071d1d5ec70f99f9289
Jayasree Balasubramanian <jayasreebalasubramanian31@gmail.com>
2026-07-28 21:45:29 +0530

    Sumofarray.java

---
+++ Java/Sumofarray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class sumofarray{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter value of n: ");
+        int n = s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        int sum=0;
+        for (int i = 0; i < n; i++)
+         {
+                sum+=arr[i];
+         }
+         System.out.println(sum);
+    }
+}
